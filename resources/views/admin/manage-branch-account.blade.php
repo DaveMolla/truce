ebd33c06f4e04bc773a0e4a5200028408ecb634b
DaveMolla <dawitmolla98@gmail.com>
2024-05-29 12:15:22 +0300

    add import side bar

--- resources/views/admin/manage-branch-account.blade.php
+++ resources/views/admin/manage-branch-account.blade.php
@@ -43,6 +43,9 @@
                         <a href="{{ route('admin.manage-branch-account') }}" class="text-lg hover:text-gray-400">Manage
                             Branch Account</a>
                     </li>
+                    <li class="mb-4">
+                        <a href="{{ route('bingo-cards.import-form') }}" class="text-lg hover:text-gray-400">Import Bingo Cards</a>
+                    </li>
                 </ul>
             </nav>
         </div>
