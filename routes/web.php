1de0d7fc02fd5df05a946c640d8eb96db1755c66
DaveMolla <dawitmolla98@gmail.com>
2024-06-17 22:54:29 +0300

    test

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,7 @@ Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified']
     Route::post('admin/top-up-agent', [AdminController::class, 'topUpAgent'])->name('admin.top-up-agent');
     Route::post('admin/withdraw-agent', [AdminController::class, 'withdrawAgent'])->name('admin.withdraw-agent');
 
+    
     Route::get('admin/manage-super-agent-account', [AdminController::class, 'manageSuperAgentAccount'])->name('admin.manage-super-agent-account');
     Route::post('admin/top-up-super-agent', [AdminController::class, 'topUpSuperAgent'])->name('admin.top-up-super-agent');
     Route::post('admin/withdraw-super-agent', [AdminController::class, 'withdrawSuperAgent'])->name('admin.withdraw-super-agent');
