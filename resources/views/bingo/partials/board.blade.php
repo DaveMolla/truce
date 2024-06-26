440923157047d1a4b414dac3625f54722b65e58c
DaveMolla <dawitmolla98@gmail.com>
2024-06-26 10:07:36 +0300

    revert the call method to call when it reaches 0 instead of 1 because it is so fast

--- resources/views/bingo/partials/board.blade.php
+++ resources/views/bingo/partials/board.blade.php
@@ -674,11 +674,11 @@
                             alert('Maximum number of calls reached.');
                             return;
                         }
-                        if (countdown > 1) {
+                        if (countdown > 0) {
                             countdown -= 1;
                             countdownDisplay.textContent = `${countdown}s`;
                         }
-                        if (countdown <= 1 && !
+                        if (countdown <= 0 && !
                             isFetching) { // Only fetch next number if not already fetching
                             fetchNextNumber();
                         }
